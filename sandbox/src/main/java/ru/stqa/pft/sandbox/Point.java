6b3478a6ee7944961cda913c51bfadf4a45473ca
Olga Belyaeva <olga.belyaeva@t-systems.com>
2020-09-17 22:03:25 +0200

    tests for modification added

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,26 +2,20 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-    public double a;
-    public double b;
+    public double x;
+    public double y;
 
 
-    public Point(double a,double b){
-        this.a = a;
-        this.b = b;
-
+    public Point  (double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static double distance (Point p1, Point p2) {
-        return Math.sqrt (Math.pow(( p2.a - p1.a ), 2) + (Math.pow(( p2.b - p1.b ), 2 )));
+    public double distance (Point) {
 
-    }
 
-    public static void main(String[] args) {
+        return Math.sqrt(Math.pow((p2.x - this.x), 2) + (Math.pow((p2.y - this.y), 2)));
 
-        Point p1 = new Point (1,1);
-        Point p2 = new Point (2,2);
-        System.out.println ("Расстояние между точками равно  " + distance(p1, p2) );
     }
 
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,10 +9,6 @@ public class PointTests {
     @Test
     public void testDistance () {
 
-        Point p1 = new Point (1,1);
-        Point p2 = new Point (2,2);
-        Assert.assertEquals(p1.distance(p1,p2), 1.4142135623730951 );
-        Assert.assertNotEquals(Math.signum(p1.distance(p1,p2)), -1.0 );
 
 
 
