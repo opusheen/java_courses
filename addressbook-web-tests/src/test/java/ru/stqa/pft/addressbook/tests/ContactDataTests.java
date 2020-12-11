848fcafd6c3f58c04bee74f2a7ce9b655f555806
Olga Belyaeva <olga.belyaeva@t-systems.com>
2020-12-12 00:07:52 +0300

    homework 5.11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDataTests.java
@@ -9,7 +9,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactPhoneTests extends TestBase {
+public class ContactDataTests extends TestBase {
 
     @Test
     public void testContactPhones() {
@@ -29,7 +29,7 @@ public class ContactPhoneTests extends TestBase {
                 equalTo(mergeEmails(contactInfoFromEditForm)));
     }
 
-    @Test 
+    @Test
     public void testContactAddress() {
         app.goTo().homePage();
         ContactData contact = app.contact().all().iterator().next();
@@ -43,12 +43,12 @@ public class ContactPhoneTests extends TestBase {
     private String mergePhones(ContactData contact) {
        return Arrays.asList(contact.getHomephone(), contact.getMobilephonenumber(), contact.getWorkphone())
                 .stream().filter((s) -> ! s.equals(""))
-                .map(ContactPhoneTests::cleaned)
+                .map(ContactDataTests::cleaned)
                 .collect(Collectors.joining("\n"));
     }
     private String mergeEmails(ContactData contact){
         return Arrays.asList(contact.getEmail(),contact.getEmail2(),contact.getEmail3()).stream().filter((s) -> ! s.equals(""))
-                .map(ContactPhoneTests::cleaned)
+                .map(ContactDataTests::cleaned)
                 .collect(Collectors.joining("\n"));
     }
 
